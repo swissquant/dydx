e51330456f28ed633b8c1cd3bfb2a09dcf0c9fe9
swissquant@gmail.com <aym.germain@gmail.com>
2022-11-22 18:30:28 +0000

    :wrench: add fetch_account

--- dydx/__init__.py
+++ dydx/__init__.py
@@ -1,2 +1,9 @@
 from .ws import WS_Trades, WS_Account, WS_Order_Book  # NOQA: F401
-from .rest import Executor, create_market_order, create_limit_order, fetch_position, fetch_positions  # NOQA: F401
+from .rest import (  # NOQA: F401
+    Executor,
+    create_market_order,
+    create_limit_order,
+    fetch_position,
+    fetch_positions,
+    fetch_account,
+)
