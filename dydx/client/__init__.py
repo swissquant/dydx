88cdbf780f21c04fc7f7dfdde352fced720dce90
swissquant@gmail.com <aym.germain@gmail.com>
2022-12-02 15:43:31 +0000

    :wrench: easy access to create_private_client

--- dydx/client/__init__.py
+++ dydx/client/__init__.py
@@ -1,3 +1,3 @@
 from .client import client  # NOQA: F401
 from .rate_limit import rate_limits  # NOQA: F401
-from .client_priv import client_priv, generate_timestamp, sign  # NOQA: F401
+from .client_priv import client_priv, generate_timestamp, sign, create_private_client  # NOQA: F401
